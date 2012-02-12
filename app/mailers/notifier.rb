b97ca5428a29f5501385a34febdb5df01df3e79e
Amir Grozki <amirgrozki@gmail.com>
2012-02-12 06:15:28 +0200

    That damn mailer.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
 class Notifier < ActionMailer::Base
-  default :from => "no-reply@ishunator.org.il"
+  FROM = "no-reply@ishunator.org.il"
+  
+  default :from => FROM
   
   add_template_helper(HomeHelper)
   
@@ -11,7 +13,8 @@ class Notifier < ActionMailer::Base
     
     recipients = @complaint.city.complaint_recipients
     
-    return mail( :to => recipients.select { |recipient| recipient.recipient_type == ComplaintRecipient::RecipientTypes::TO }.map(&:email),
+    return mail( :from => FROM,
+          :to => recipients.select { |recipient| recipient.recipient_type == ComplaintRecipient::RecipientTypes::TO }.map(&:email),
           :cc => recipients.select { |recipient| recipient.recipient_type == ComplaintRecipient::RecipientTypes::CC }.map(&:email),
           :bcc => recipients.select { |recipient| recipient.recipient_type == ComplaintRecipient::RecipientTypes::BCC }.map(&:email),
           :subject => "תלונה בדבר עישון במקומות ציבוריים במקום #{@complaint.business_name}, #{@complaint.event_date.strftime("%d/%m/%Y")}",
