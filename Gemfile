b273e9a8b95a674a65e066ba1beb8b8fd7e01e71
Amir Grozki <amirgrozki@gmail.com>
2012-02-12 04:29:57 +0200

    Preparing for deployment to Heroku servers.

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'rdiscount'
 
 gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 
+gem 'aws-s3'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/complaint_image.rb
+++ app/models/complaint_image.rb
@@ -1,7 +1,15 @@
 class ComplaintImage < ActiveRecord::Base
   belongs_to(:complaint)
   
-  has_attached_file(:image, :styles => { :thumbnail => ['120x80>', :jpg], :normal => ['800x600>', :jpg] })
+  has_attached_file(:image, 
+    :storage => :s3,
+    :bucket => 'media.ishunator',
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENC['S3_SECRET']
+    },
+    :styles => { :thumbnail => ['120x80>', :jpg], :normal => ['800x600>', :jpg],
+  })
   
   validates_attachment_presence(:image)
   validates_attachment_content_type(:image, :content_type => ['image/png', 'image/x-png', 'image/jpg', 'image/jpeg', 'image/pjpeg'])
