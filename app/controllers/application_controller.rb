9923c3a69b06e727c979d8b4f133aa9f4e21f4bf
ik5 <idok@linesip.com>
2012-08-10 20:59:59 +0300

    Temporary disabled 404, to better error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  rescue_from(ActionController::RoutingError, ActiveRecord::RecordNotFound) do |e|
-    render :file => "#{Rails.root}/public/404.html", :status => :not_found
-  end
+  #rescue_from(ActionController::RoutingError, ActiveRecord::RecordNotFound) do |e|
+  #  render :file => "#{Rails.root}/public/404.html", :status => :not_found
+  #end
   
 protected  
-  def not_found
-    raise ActionController::RoutingError.new('Not Found')
-  end
+#  def not_found
+#    raise ActionController::RoutingError.new('Not Found')
+#  end
 end
