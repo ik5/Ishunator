e43e67a27710847abd51136b49039c7d80b705df
ik5 <idok@linesip.com>
2012-08-18 14:27:34 +0300

    Moved Hebrew to external file, now Ruby should not yell at me

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,57 +2,58 @@
 
 class Notifier < ActionMailer::Base
   default :from => "no-reply@avir-naki.com"
-  
+
   default_url_options[:host] = 'furious-window-9057.herokuapp.com'
-  
+
   layout 'mail'
-  
+
   add_template_helper(HomeHelper)
-  
+
   def confirm(complaint)
     @complaint = complaint
-    
+
     filename = "complaint_#{complaint.to_param}.pdf"
-    
+
     attachments[filename] = generate_pdf(filename)
-    
+
     return mail( :to => complaint.statement_email,
-          :subject => "אישור תלונתך מספר #{complaint.to_param} בדבר עישון במקומות ציבוריים",
+          :subject => t('subject', :number => complaint.to_param),
           :template_path => 'mailer',
           :template_name => 'confirm' )
   end
-  
+
   def complaint(complaint)
-    
+
     @complaint = complaint
-    
+
     global_recipients = ComplaintRecipient.global_recipients
-    
+
     recipients = global_recipients.concat( @complaint.city.complaint_recipients )
-    
+
     to = recipients.select { |r| r.recipient_type == ComplaintRecipient::RecipientTypes::TO }.map(&:email)
     cc = recipients.select { |r| r.recipient_type == ComplaintRecipient::RecipientTypes::CC }.map(&:email)
     bcc = recipients.select { |r| r.recipient_type == ComplaintRecipient::RecipientTypes::BCC }.map(&:email)
-    
+
     filename = "complaint_#{complaint.to_param}.pdf"
-    
+
     attachments[filename] = generate_pdf(filename)
-    
+
     return mail( :to => to,
+          :from => complaint.statement_email,
           :cc => cc,
           :bcc => bcc,
-          :subject => "תלונה בדבר עישון במקומות ציבוריים במקום #{@complaint.business_name}, #{@complaint.event_date.strftime("%d/%m/%Y")}",
+          :subject => t('subject2', :number => @complaint.business_name, :date => @complaint.event_date.strftime("%d/%m/%Y")),
           :template_path => 'mailer',
           :template_name => 'show' )
 
   end
-  
+
 private
   def generate_pdf(filename)
-    
-    pdf = render_to_string(:pdf => filename, :template => 'home/show.html.haml', :layout => 'pdf.html.haml', :encoding => 'UTF-8') 
-    
+
+    pdf = render_to_string(:pdf => filename, :template => 'home/show.html.haml', :layout => 'pdf.html.haml', :encoding => 'UTF-8')
+
     return WickedPdf.new.pdf_from_string(pdf)
-    
+
   end
 end
