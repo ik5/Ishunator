e6ee7b3ca851ab21b3cffa9e7909c36a77ecc7a6
ik5 <idok@linesip.com>
2012-08-18 14:57:37 +0300

    Moved Hebrew from source code to external file, so Ruby will stop crashing

--- app/models/complaint_recipient.rb
+++ app/models/complaint_recipient.rb
@@ -4,25 +4,25 @@ require "email_validator.rb"
 
 class ComplaintRecipient < ActiveRecord::Base
   belongs_to(:city)
-  
+
   validates(:description, :email, :recipient_type, :presence => true)
   validates(:email, :email => true)
-  
+
   scope :global_recipients, where(:city_id => nil)
-  
+
   def as_json(options = {})
     return super.merge({ :recipient_type_name => self.recipient_type_name })
   end
-  
+
   def recipient_type_name
     return RecipientTypes::Types[recipient_type]
   end
-  
+
   class RecipientTypes
     TO = 0
     CC = 1
     BCC = 2
-    
-    Types = { TO => 'נמען', CC => 'עותק', BCC => 'עותק נסתר' }
+
+    Types = { TO => I18n.t('to'), CC =>  I18n.t('cc'), BCC => I18n.t('bcc') }
   end
 end
