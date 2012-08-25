a895f9fdea68222fdc312bbaccc2748658920f63
ik5 <idok@linesip.com>
2012-08-25 20:43:09 +0300

    Rewrote few things, because ruby did not like it, it had too deep exception

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,18 +1,14 @@
 class AdminUser < ActiveRecord::Base
 
-  def password=(pass)
-    self.password = phash(pass)
-  end
-
-  def self.phash(pass)
-    Digest::SHA1.hexdigest(pass)
+  def pass=(pass)
+    self.password = Digest::SHA1.hexdigest(pass)
   end
 
   def self.credentials(username, password)
-    find_by_username_and_password(username, phash(password))
+    find_by_username_and_password(username, Digest::SHA1.hexdigest(password))
   end
 
   def self.auth?(username, password)
-    credentials(username, password) != nil
+    find_by_username_and_password(username, Digest::SHA1.hexdigest(password)) != nil
   end
 end
