a50c1f05efefb1d1dcc3e331984c9e4314a13dd5
ik5 <idok@linesip.com>
2012-08-25 11:59:27 +0300

    Line ending

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,18 +1,18 @@
 class Admin::AdminController < ApplicationController
   REALM = 'ISHUNATOR'
-  
+
   before_filter(:authenticate)
-  
+
 private
   def authenticate
     unless session[:admin]
       success = authenticate_with_http_basic do |username, password|
           env = ENV[ [REALM, username].join('_').upcase ]
           compare = Rails.env.development? ? 'password' : (env || false)
-          
+
           return password if compare == password
       end
-      
+
       if success
         session[:admin] = true
         return true
