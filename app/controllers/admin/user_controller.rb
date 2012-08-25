17de7219a647cd04d4bd949a519a1185c17e6400
ik5 <idok@linesip.com>
2012-08-25 17:03:52 +0300

    New code for working with admin user

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -1,2 +1,21 @@
 class Admin::UserController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+    username = params[:username]
+    password = params[:password]
+
+    user = find_or_initialize_by_username(username)
+    user.password=password
+    user.save!
+    flash[:info] = "The user %{username} was saved properly"
+  rescue
+    flash[:error] = 'Could not save user'
+  ensure
+    redirect_to admin_user_index_path
+  end
+
 end
