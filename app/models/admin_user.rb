d57857e72bd40c79e0dbab9271e64a65feca7584
ik5 <idok@linesip.com>
2012-08-25 17:04:22 +0300

    Some methods to help work with the sha1 hash

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,18 @@
 class AdminUser < ActiveRecord::Base
 
   def password=(pass)
-    self.password = Digest::SHA1.hexdigest(pass)
+    self.password = phash(pass)
+  end
+
+  def self.phash(pass)
+    Digest::SHA1.hexdigest(pass)
+  end
+
+  def self.credentials(username, password)
+    find_by_username_and_password(username, phash(password))
   end
 
   def self.auth?(username, password)
-    find_by_username_and_password(username, Digest::SHA1.hexdigest(password)) != nil
+    credentials(username, password) != nil
   end
 end
