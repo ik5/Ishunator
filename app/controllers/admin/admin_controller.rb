c50c6080a8e3cae5b0d196a1d39dee2f6b1ba816
ik5 <idok@linesip.com>
2012-08-25 14:01:04 +0300

    Making the username and password of admin from database rather then hardcoded

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -8,14 +8,15 @@ private
     unless session[:admin]
       success = authenticate_with_http_basic do |username, password|
           env = ENV[ [REALM, username].join('_').upcase ]
-          compare = Rails.env.development? ? 'password' : (env || false)
+          #compare = Rails.env.development? ? 'password' : (env || false)
 
-          return password if compare == password
+          #return password if compare == password
+          return AdminUser.auth?(username, password)
       end
 
       if success
         session[:admin] = true
-        return true
+        true
       else
         request_http_basic_authentication
       end
